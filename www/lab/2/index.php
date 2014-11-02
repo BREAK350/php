6fed59222f75ae2d2d04e1f4787c479f56c04415
Dima Lymarenko <break350@gmail.com>
2014-11-02 19:51:56 +0200

    змінив файл для лаби 2 відповідно до змін функціоналу.

--- www/lab/2/index.php
+++ www/lab/2/index.php
@@ -3,16 +3,18 @@
 Створити форму для добавлення нового запису у файл і реалізувати її обробку.
 -->
 <?php
-require_once $dir_functional . 'addData.php';
+require_once $dir_functional . 'model/file/file.php';
+require_once $dir_functional . 'model/model.php';
+require_once $dir_functional . 'view/view.php';
 if (isset ( $_POST ['submit'] )) {
-	if (($res = addData ()) !== true) {
-		?>
-<h2><?php echo $res;?></h2>
-<?php
+	$ass_array = readFromPOST_createArray ();
+	if ($ass_array === false) {
+		echo "<h2>Не вдалося записати дані у файл!</h2>";
+	} else {
+		saveToFile ( $_SERVER ['DOCUMENT_ROOT'] . '/../data/monitors.txt', $ass_array );
 	}
 }
-// створюємо двовимірний масив, рядками якого є записи, а стовпці - атрибутами.
-include $dir_functional . 'load from file/index.php';
-// відображаємо дані.
-include $dir_functional . 'lab/2/index.php';
+$monitors = loadFromFile ( $dir_data . "monitors.txt" );
+printTable ( $monitors );
+include $dir_functional . 'view/form.php';
 ?>
